ea5f74b8f70b8e0fb9c486784e58dbff913cc9e6
GitheTrunk <bsengleang@gmail.com>
2025-11-12 12:56:12 +0700

    Initial commit - Vue + Tailwind + Supabase setup

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <h1 class="text-5xl font-bold text-blue-600 mb-4">Testing setup 🏠</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
