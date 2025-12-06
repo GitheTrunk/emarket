101b51ff15c9cf820f3abeaf1ae09f35e78f6aa1
GitheTrunk <bsengleang@gmail.com>
2025-12-06 13:16:01 +0700

    fix: resolve layout to make it clean

--- frontend/src/components/layout/BuyerNavbar.vue
+++ frontend/src/components/layout/BuyerNavbar.vue
@@ -29,7 +29,7 @@
 <script setup>
 
 import "primeicons/primeicons.css";
-import SearchBar from "../others/SearchBar.vue";
+import SearchBar from "../ui/SearchBar.vue";
 import { ref } from "vue";
 const searchQuery = ref("");
 const handleSearch = (query) => {
