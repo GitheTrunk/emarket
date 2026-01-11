b7eedf5884d1e2c491c7245449dc3271540c2fe4
GitheTrunk <bsengleang@gmail.com>
2026-01-11 22:50:11 +0700

    fix: resolve icons.js file

--- frontend/src/assets/icons.js
+++ frontend/src/assets/icons.js
@@ -1,5 +1,4 @@
 import {
-<<<<<<< HEAD
   FaMobileScreenButton,
   FaDesktop,
   FaClock,
@@ -19,25 +18,4 @@ OhVueIcon.addIcons(
   FaGamepad,
   FaArrowLeft,
   FaArrowRight
-=======
-    FaMobileScreenButton,
-    FaDesktop,
-    FaClock,
-    FaCamera,
-    FaHeadphones,
-    FaGamepad,
-    FaArrowLeft,
-    FaArrowRight
-} from "oh-vue-icons/icons";
-
-OhVueIcon.addIcons(
-    FaMobileScreenButton,
-    FaDesktop,
-    FaClock,
-    FaCamera,
-    FaHeadphones,
-    FaGamepad,
-    FaArrowLeft,
-    FaArrowRight
->>>>>>> 979c377e6514d35a74cbbc32dc7bef2319efbbbc
 );
